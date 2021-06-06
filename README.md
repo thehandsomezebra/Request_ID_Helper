bfc2ec8d0536ed3405675ca2d8d42e0da3a4b54d
Stephanie Frantz <stephanie.frantz@cengage.com>
2021-06-06 00:34:59 -0400

    updates and tweaks

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 > You will need to use the Evernote Desktop App.  If you don't have this installed, please [visit Evernote's site and download it](https://evernote.com/download)
 
+---
+
 ### First Time Setup
 
 1. Hit `Command` and `Spacebar`
@@ -16,6 +18,8 @@ cd ~/Desktop && curl https://raw.githubusercontent.com/thehandsomezebra/Request_
 
 4. You are ready to go!
 
+---
+
 ## How to use this tool
 
 1. Open the Evernote app.
@@ -25,7 +29,7 @@ cd ~/Desktop && curl https://raw.githubusercontent.com/thehandsomezebra/Request_
 ![Screenshot showing step 3](images/2.png)
 4. Be sure this file is going into your Downloads folder! (This location is important!)
 ![Screenshot showing step 4](images/3.png)
-5. Did you download & prep the script permissions yet?  If you haven't done that, I would recommend scrolling up to the First Time Setup.  You'll need it to keep going.
+5. Did you follow the steps in the setup?  If you haven't done that, I would recommend scrolling up to the First Time Setup and going thru the steps.  You'll need it to keep going.
 6. Now that you have your export file in the *Downloads* folder, and you have the _script.sh_ file... go ahead and click on `script.sh` (which is located on your Desktop) to run it.
 7. It will open a terminal and prompt you to pick the correct file -- just type the corresponding number and hit Enter.
 8. The script do some magic in the background, and then drop a new file on your Desktop called `output_<timestamp>.csv`
@@ -33,6 +37,19 @@ cd ~/Desktop && curl https://raw.githubusercontent.com/thehandsomezebra/Request_
 
 
 🐙 I hope this helps!
+
+
+
+
+---
+
+## Troubleshooting
+
+- Did you get a bunch of scripty text when you attempted to run it the first time?  
+  - Try right/option clicking on the `script.sh` file and choose "Open With >" `Terminal`
+
+- Did you get an error about a file you chose because it had a space in it?
+  - Simply rename your file so that it doesn't have a space in it.  Try no spaces or underscores instead.
 
 
 
--- script.sh
+++ script.sh
@@ -6,9 +6,9 @@ echo "Searching for latest exports in your Downloads folder..."
 echo ""
 FILESFOUND="/tmp/filesfound.txt"
 echo -n >$FILESFOUND
-if [ `find ~/Downloads/ -name "*.enex" | wc -l` -gt 0 ] ; then
+if [ $(find ~/Downloads/ -name "*.enex" | wc -l) -gt 0 ]; then
 
-ls -d ~/Downloads/*.enex >$FILESFOUND 
+  ls -d ~/Downloads/*.enex >$FILESFOUND
 
 fi
 
@@ -19,67 +19,116 @@ if [ -s $FILESFOUND ]; then
   read SELECTION
   theExportFile=$(sed -n ${SELECTION}p $FILESFOUND)
   echo ""
-  echo "Great choice!"
-  echo ""
   echo "You picked: $theExportFile "
+
+  #check that the file has no spaces
+  hasSpaces=$(echo "$theExportFile" | grep " " -c)
+  if [ $hasSpaces -gt 0 ]; then
+
+    echo "Oh dear, we can't use a filename with a space in it."
+    echo "Would you mind changing the filename to have no spaces?"
+    echo ""
+    echo "Once you do that, you can come back and try again."
+    echo ""
+
+    ###
+    counter=10
+    echo "This screen will close in $counter seconds!"
+    until [ $counter -lt 1 ]; do
+      printf "$counter..."
+      sleep 1
+      ((counter--))
+    done
+    printf "Bye!"
+  ###
+
+  fi
+
+  echo ""
+  echo "Great choice!"
+
   printf "Doing cool things....."
 
+  # FILE=$(echo "$theExportFile" | sed 's! !\\ !g')
   FILE=$theExportFile
+  # echo $FILE
   TEMPORARY="/tmp/holding.txt"
-  grep -o '<title.*title>\|^--.*' $FILE | sed -e s/\<title\>//g | sed -e s/\<\\/title\>//g >$TEMPORARY
-  
-  NOW=$(date +"%m-%d-%Y_%k-%M-%S")
+  grep -o "<title.*title>\|^--.*" "$FILE" | sed -e s/\<title\>//g | sed -e s/\<\\/title\>//g >$TEMPORARY
+
+  NOW=$(date +"%-m-%-d-%-Y_%-k-%-M-%-S")
 
   OUTPUTFILE=~/Desktop/output_$NOW.csv
-
-  echo "Request ID List" > $OUTPUTFILE
+  # echo $OUTPUTFILE
+  # echo "Request ID List" >$OUTPUTFILE
+  echo -n >$OUTPUTFILE
 
   cat $TEMPORARY |
     while IFS=$'\n' read -r line; do
-      echo $line | grep -o 'Request ID#:\d\+' >>$OUTPUTFILE
+      ##and now we'll need to remove the request id after we grep it
+      # |  sed -e s/"Request ID#:"//g
+      echo $line | grep -o 'Request ID#:\d\+' | sed -e s/"Request ID#:"//g >>$OUTPUTFILE
     done
 
-printf "..." && sleep 1
+  printf "..." && sleep 1
 
   printf "\nLooking for duplicates..."
   dupeCount=$(sort $OUTPUTFILE | uniq -d | wc -l)
 
-printf "..." && sleep 1
+  printf "..." && sleep 1
 
   if [ $dupeCount -gt 0 ]; then
-  echo ""
+    echo ""
     echo "   Uh oh, looks like we found some duplicates."
     printf "   We should clean those up..."
     sort -u $OUTPUTFILE -o $OUTPUTFILE
     sleep 1
+
     printf "..."
     echo "   Awesome, all cleaned up."
   else
     echo "   Cool, no duplicate entries found."
   fi
+  totalCount=$(cat $OUTPUTFILE | wc -l)
+  echo "Adding a header to the csv"
+
+  echo -e "Request_ID_List\n$(cat $OUTPUTFILE)" >$OUTPUTFILE
+
   echo -ne "\n\n"
   echo "We are all done!"
   echo "I put the file on your Desktop."
   echo "See $OUTPUTFILE"
   echo ""
-  echo "It has a total of $(cat $OUTPUTFILE | grep "Request ID#:" -c) entries."
+  echo "It has a total of $totalCount entries."
   echo ""
   echo "I hope you have an awesome day!"
+
+  ###
+  counter=10
+  echo "This screen will close in $counter seconds!"
+  until [ $counter -lt 1 ]; do
+    printf "$counter..."
+    sleep 1
+    ((counter--))
+  done
+  printf "Bye!"
+###
 
 else
 
   echo "Uh oh, looks like you didn't export a notebook yet."
-  echo "Please see www------ to find out how to do this"
+  echo "Please see https://github.com/thehandsomezebra/Request_ID_Helper to find out how to do this"
   echo "             ^^^^ (cmd + click on that url)"
   echo ""
 
-fi
+  ###
+  counter=10
+  echo "This screen will close in $counter seconds!"
+  until [ $counter -lt 1 ]; do
+    printf "$counter..."
+    sleep 1
+    ((counter--))
+  done
+  printf "Bye!"
+###
 
-counter=10
-echo "This screen will close in $counter seconds!"
-until [ $counter -lt 1 ]; do
-  printf "$counter..."
-  sleep 1
-  ((counter--))
-done
-printf "Bye!"
+fi
